2b16b807a5ffa341f11a84f8d174b69f565c59d7
diego-rivera820 <diego.gomezriver@campusucc.edu.co>
2026-02-17 02:04:52 +0000

    fin del ejercicio 1

--- season_2/Taller_25.java
+++ season_2/Taller_25.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Taller_25 {
     
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero de elementos del arreglo: ");
+        int n = sc.nextInt();
+
+        int [] a = new int[n];
+
+        for (int i = 0; i < a.length; i++){
+            System.out.println("Ingrese el elemento " + (i+1) + ": ");
+            a[i] = sc.nextInt();
+        }
+
+        int total = 0;
+        for(int i = 0; i < a.length; i++){
+            total += a[i];
+        }
+        System.out.println("La suma de los elementos del arreglo es: " + total);
+
+    }
 }
